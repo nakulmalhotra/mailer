9729f7fa4b83c3baef1ba7a6d37c48b6a31958c6
nakulmalhotra <nakulmalhotra1607@gmail.com>
2017-06-23 15:31:43 +0530

    Add files via upload

---
+++ fake-mail.php
@@ -0,0 +1,15 @@
+<?php
+
+$to_email = $_REQUEST['email'];
+$name = $_REQUEST['name'];
+$sender_email = $_REQUEST['sender'];
+$subject = $_REQUEST['subject'];
+$content = $_REQUEST['content'];
+
+$header = "From: $name"."<$sender_email>/r/n";
+
+mail($to_email,$subject,$content,$header);
+
+echo "Sent successfully";
+
+?>
